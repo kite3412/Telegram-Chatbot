3099c22f14e8b91de33b58e5bd06f083d6da4029
Bowen <148347326+kite3412@users.noreply.github.com>
2025-05-13 12:50:05 +0000

    1

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 # Gemini
 
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=["Get", "Post"])
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()
