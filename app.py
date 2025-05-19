42e454e1376a29b487f0148131a3665398824a30
Bowen <148347326+kite3412@users.noreply.github.com>
2025-05-19 13:34:17 +0000

    1

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, request, render_template
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+import sqlite3
+import datetime
 
 load_dotenv()
 
@@ -14,9 +16,26 @@ model = genai.GenerativeModel("gemini-2.0-flash")
 
 app = Flask(__name__)
 
-@app.route("/", methods=["Get", "Post"])
+first_time = 1
+
+@app.route("/", methods=["Get", "Post"]) # start point
 def index():
     return(render_template("index.html"))
+
+@app.route("/main", methods=["Get", "Post"])
+def main():
+    global first_time
+    if first_time == 1:
+        name = request.form.get("q")
+        t = datetime.datetime.now()
+        conn = sqlite3.connect('user.db')
+        c = conn.cursor()
+        c.execute("insert into users(name, timestamp) values(?,?)", (name, t))
+        conn.commit()
+        c.close()
+        conn.close()
+        first_time = 0
+    return(render_template("main.html"))
 
 @app.route("/gemini",methods=["GET","POST"])
 def gemini():
@@ -28,6 +47,28 @@ def gemini_reply():
     r = model.generate_content(q)
     r = r.text
     return(render_template("gemini_reply.html", r=r))
+
+@app.route("/user_log",methods=["GET","POST"])
+def user_log():
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("select * from users")
+    r=""
+    for row in c:
+        r= r+str(row)
+    c.close()
+    conn.close()
+    return(render_template("user_log.html", r=r))
+
+@app.route("/delete_log",methods=["GET","POST"])
+def delete_log():
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("delete from users")
+    conn.commit()
+    c.close()
+    conn.close()
+    return(render_template("delete_log.html"))
 
 if __name__ == "__main__":
     app.run()
